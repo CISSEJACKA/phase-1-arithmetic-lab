472d0380e41d9972b302f924a7ad013286102a6b
JACKA <jackarcisse@gmail.com>
2024-03-19 09:12:15 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 100);
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+let numbers = [10, 15, 20, 5];
+let max = Math.max(...numbers);
+      
 
-// Write your code here
+
+
